a772dc862fe89e24c04fa46d3006961e2e511e23
Nathan Stangler <123645803+NathanStangler@users.noreply.github.com>
2025-04-11 11:19:38 -0500

    Update BOOK.sql
    Updated relational schema does not have a PUBLISHER entity since it was not meant to be an entity in the first assignment

--- entity/BOOK.sql
+++ entity/BOOK.sql
@@ -2,9 +2,11 @@ CREATE TABLE BOOK (
     BookID INT PRIMARY KEY,
     Title VARCHAR(255) NOT NULL,
     Genre VARCHAR(50),
-    Price DECIMAL(10, 2),
-    StockQuantity INT,
+    Price DECIMAL(10, 2) CHECK (Price > 0),
+    StockQuantity INT CHECK (StockQuantity >= 0),
     PublicationYear YEAR,
-    PublisherID INT,
-    FOREIGN KEY (PublisherID) REFERENCES PUBLISHER(PublisherID)
+    PublisherFirstName VARCHAR(50),
+    PublisherLastName VARCHAR(50),
+    PublisherEmail VARCHAR(100) UNIQUE,
+    PublisherPhoneNumber CHAR(10) UNIQUE
 );
